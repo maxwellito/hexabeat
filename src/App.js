1d68953ed5062ff72e25e960bcfc9eedb791a6c0
maxwellito <duluc.maxime@gmail.com>
2018-07-03 00:03:26 +0100

    Clean up source

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
-import TiledLines from './components/useless/tiledLines';
-import LevelMeter from './components/track/levelMeter/levelMeter';
-import TrackGrid from './components/track/trackGrid/trackGrid';
-import logo from './logo.svg';
-import './App.css';
+import Track from './components/track/track';
 
+/**
+ * Fun times:
+ * 
+ * <TiledLines gap={18} width={800} height={500} />
+ */
 class App extends Component {
   render() {
-    let trackData = [
-      [0, 1, 1, 1,  0, 0, 0, 1,  1, 0, 1, 0,  1, 1, 0, 0],
-      [0, 1, 0, 0,  1, 1, 0, 0,  0, 1, 1, 1,  0, 1, 1, 0]
-    ]
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <LevelMeter progress={.6}/>
-          <TiledLines gap={18} width={800} height={500} />
-          <TrackGrid currentPos={12} data={trackData} />
-        </p>
+      <div>
+        <Track/>
       </div>
     );
   }
--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import LevelMeter from './levelMeter/levelMeter';
+import TrackGrid from './trackGrid/trackGrid';
 import './track.css';
 
 class Track extends Component {
 
-  render() {
+  constructor() {
+    super();
+    this.setState({currentPos: 0})
+  }
 
+  render() {
+    let currentPos = (this.state && this.state.currentPos) || 0,
+        trackData = [
+      [0, 1, 1, 1,  0, 0, 0, 1,  1, 0, 1, 0,  1, 1, 0, 0],
+      [0, 1, 0, 0,  1, 1, 0, 0,  0, 1, 1, 1,  0, 1, 1, 0]
+    ]
+
+    setTimeout(() => {
+      this.setState({currentPos: (currentPos + 1) % 16})
+    }, 2000);
 
     return (
-      <div>
-        
+      <div className='track'>
+        <LevelMeter progress={.6}/>
+        <TrackGrid currentPos={currentPos} data={trackData} />
       </div>
     );
   }
