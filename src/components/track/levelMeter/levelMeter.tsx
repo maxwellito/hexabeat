5e5ae06bef636c1092571676c4a2bf2415a5afd4
maxwellito <duluc.maxime@gmail.com>
2019-02-20 18:55:46 +0000

    Kinda ok design, really need to move on

--- src/components/track/levelMeter/levelMeter.tsx
+++ src/components/track/levelMeter/levelMeter.tsx
@@ -1,20 +1,23 @@
-import * as React from 'react';
-import './levelMeter.css';
+import * as React from "react";
+import "./levelMeter.css";
 
 export interface LevelMeterProps {
-  active?: boolean,
-  progress: number
+  active?: boolean;
+  progress: number;
 }
 
 export class LevelMeter extends React.Component<LevelMeterProps> {
-
   render() {
-    let height = Math.round((this.props.progress || 0) * 100) + '%',
-        wrapClass = 'level-meter ' + (this.props.active ? 'active' : '')
+    let height = Math.round((this.props.progress || 0) * 100) + "%",
+      wrapClass = "level-meter " + (this.props.active ? "active" : "");
     return (
       <div className={wrapClass}>
-        <div className='level-meter-wrap' style={{'height': height}}></div>
-        <span className='level-meter-content'>{this.props.progress.toFixed(2).substr(2)}</span>
+        <div className="level-meter-wrap">
+          <div className="level-meter-content" style={{ height: height }} />
+        </div>
+        <span className="level-meter-label">
+          {this.props.progress.toFixed(2).substr(2)}
+        </span>
       </div>
     );
   }
