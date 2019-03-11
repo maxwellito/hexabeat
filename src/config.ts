e722faa178d67a274357515e8dcea0799df56bac
maxwellito <duluc.maxime@gmail.com>
2019-03-11 01:10:08 +0000

    Add a config files for static data

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+/**
+ * Here is the base config
+ * Where all the default configuration
+ * is defined.
+ */
+
+export default {
+  livesets: ['/livesets/minimal.json', '/livesets/panorama.json'],
+  repositories: ['samaaron/sonic-pi', 'maxwellito/matrx']
+};
