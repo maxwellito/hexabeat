2e41dad3002048f240e8fae10da1942cacb6c57d
maxwellito <duluc.maxime@gmail.com>
2019-10-17 23:48:58 +0100

    Add liveset load from location hash

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,35 @@ import { App } from './App';
 import config from './config';
 import { Liveset } from 'models/Liveset';
 import { store, actions } from 'store';
+import themeService from 'services/Theme';
 import { Machine } from 'models/Machine';
 
 import './index.css';
 
+// CHeck if a config file is provided
+if (location.hash) {
+  // Load config file
+  const theLS = new Liveset(location.hash.substr(1));
+  theLS
+    .loadConfig()
+    .then(() => theLS.loadAssets())
+    .then(() => {
+      store.dispatch(actions.setBpm(120));
+      store.dispatch(actions.setCurrentBit(0));
+      store.dispatch(actions.setSelectedTrack(null));
+      store.dispatch(actions.setVolume(0.75));
+      store.dispatch(actions.setGitRepositories(theLS.repoData));
+      store.dispatch(actions.setLiveset(theLS));
+      themeService.set(theLS.theme);
+    });
+}
+// Load default config files
 config.livesets.forEach(livesetFilePath => {
   new Liveset(livesetFilePath).loadConfig().then(liveset => {
     store.dispatch(actions.addLiveset(liveset));
   }, console.warn);
 });
+
 new Machine();
 
 ReactDOM.render(<App />, document.getElementById('root'));
